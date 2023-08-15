016213afa2ea7bc609e0527618113b554815864a
javierabreu9 <141370044+javierabreu9@users.noreply.github.com>
2023-08-15 01:01:29 -0300

    punto8
    punto8

---
+++ eventos.js
@@ -0,0 +1,9 @@
+function mostrarMensaje() {
+    alert("Hola! Soy el div");
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    const divConBtn = document.querySelector(".divbtn");
+    divConBtn.addEventListener("click", mostrarMensaje);
+  });
+  
